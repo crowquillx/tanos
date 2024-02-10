6559a9b0d77285772060f9016c8e93d2fe79592b
Tanner Decker <tancodes@proton.me>
2024-02-09 18:43:40 -0700

    systemdboot config

--- config/system/boot.nix
+++ config/system/boot.nix
@@ -4,6 +4,9 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.consoleMode = "max";
+  
 
   # This is for OBS Virtual Cam Support - v4l2loopback setup
   boot.kernelModules = [ "v4l2loopback" ];
