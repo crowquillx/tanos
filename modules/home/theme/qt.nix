4c565ba8a432e379d43bacae8e18a595461df8dd
tan <tancodes@proton.me>
2026-02-27 22:01:07 -0700

    fix: defer qt style ownership to illogical

--- modules/home/theme/qt.nix
+++ modules/home/theme/qt.nix
@@ -1,13 +1,5 @@
-{ lib, options, vars ? { }, ... }:
-let
-  v = vars;
-  get = path: default: lib.attrByPath path default v;
-  desktopEnabled = get [ "desktop" "enable" ] true;
-  enabled = get [ "features" "theme" "qt" "enable" ] true;
-  hasIllogicalEnableOption = lib.hasAttrByPath [ "programs" "illogical-impulse" "enable" ] options;
-in
+{ ... }:
 {
-  config = lib.mkIf (desktopEnabled && enabled && !hasIllogicalEnableOption) {
-    qt.enable = true;
-  };
+  # Illogical owns Qt/session styling while that integration is enabled.
+  config = { };
 }
