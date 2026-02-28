bd2d7e156232ccb872ca432e9632706512be0741
tan <tancodes@proton.me>
2026-02-27 21:20:13 -0700

    update cachix key

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   nixConfig = {
     extra-substituters = [ "https://hyprland.cachix.org" ];
-    extra-trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+2Yllh44r0H3rNU2r7U1DQLR5vTcofmb7g=" ];
+    extra-trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
   inputs = {
