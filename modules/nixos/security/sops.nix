3ebae2a0c77c5258ad00c0af001cac8f8ec32712
tan <tancodes@proton.me>
2026-04-17 12:48:23 -0600

    fix sops: always set defaultSopsFile to avoid validation error on disabled hosts

--- modules/nixos/security/sops.nix
+++ modules/nixos/security/sops.nix
@@ -1,4 +1,8 @@
-{ lib, vars ? { }, ... }:
+{
+  lib,
+  vars ? { },
+  ...
+}:
 let
   get = path: default: lib.attrByPath path default vars;
   enabled = get [ "security" "sops" "enable" ] true;
@@ -6,17 +10,18 @@ let
   ageKeyFile = get [ "security" "sops" "ageKeyFile" ] "/var/lib/sops-nix/key.txt";
 in
 {
-  config = lib.mkIf enabled {
-    sops =
-      {
+  config = lib.mkMerge [
+    (lib.optionalAttrs (defaultSopsFile != null) {
+      sops.defaultSopsFile = defaultSopsFile;
+    })
+    (lib.mkIf enabled {
+      sops = {
         age = {
           keyFile = lib.mkForce ageKeyFile;
           sshKeyPaths = lib.mkForce [ ];
         };
         validateSopsFiles = false;
-      }
-      // lib.optionalAttrs (defaultSopsFile != null) {
-        defaultSopsFile = defaultSopsFile;
       };
-  };
+    })
+  ];
 }
